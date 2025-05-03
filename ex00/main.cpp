cc03b2d2678f60fcd1dc7fe426fe5bf1892df799
Patthh <luffinza@gmail.com>
2025-05-03 19:31:02 +0700

    restructure the file via the subject + ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,23 +6,13 @@
 /*   By: pracksaw <pracksaw@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 18:45:30 by pracksaw          #+#    #+#             */
-/*   Updated: 2025/05/03 16:16:40 by pracksaw         ###   ########.fr       */
+/*   Updated: 2025/05/03 19:19:41 by pracksaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Zombie.hpp"
+#include "Fixed.hpp"
 
 int main() 
 {
-	Zombie stackZombie(YELLOW "Stack" RES);
-	stackZombie.announce();
-	
-	randomChump(CYAN"Chump" RES);
-	randomChump(RED"Chungus" RES);
 
-	Zombie *heapZombie = newZombie(GREEN"Heap" RES);
-	heapZombie->announce();
-	delete heapZombie;
-	
-	return (0);
 }
